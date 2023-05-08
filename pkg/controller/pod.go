d94f39084d4b8d84d11af1365bb35c2932595c6c
zhangzujian <zhangzujian.7@gmail.com>
2023-05-08 17:19:04 +0800

    wip

--- pkg/controller/pod.go
+++ pkg/controller/pod.go
@@ -915,6 +915,9 @@ func (c *Controller) handleDeletePod(pod *v1.Pod) error {
 			return err
 		}
 	}
+
+	c.ipam.ReleaseAddressByPod(key)
+
 	podNets, err := c.getPodKubeovnNets(pod)
 	if err != nil {
 		klog.Errorf("failed to get pod nets %v", err)
@@ -932,7 +935,6 @@ func (c *Controller) handleDeletePod(pod *v1.Pod) error {
 			}
 		}
 	}
-	c.ipam.ReleaseAddressByPod(key)
 	for _, podNet := range podNets {
 		c.syncVirtualPortsQueue.Add(podNet.Subnet.Name)
 	}
