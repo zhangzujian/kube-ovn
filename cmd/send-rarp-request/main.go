6a91dfaa8c3c447aac0400bcf0275565a6255d2a
zhangzujian <zhangzujian.7@gmail.com>
2024-07-07 09:58:44 +0000

    wip
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- cmd/send-rarp-request/main.go
+++ cmd/send-rarp-request/main.go
@@ -16,9 +16,22 @@ const rarpProtocol = 0x8035
 const rarpRequestOp = 3
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <interface>\n", os.Args[0])
+	if len(os.Args) != 2 && len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <interface> [ip address]\n", os.Args[0])
 		os.Exit(1)
+	}
+
+	addr := netip.IPv4Unspecified()
+	if len(os.Args) == 3 {
+		var err error
+		addr, err = netip.ParseAddr(os.Args[2])
+		if err != nil {
+			util.LogFatalAndExit(err, "invalid ip address %q", os.Args[2])
+		}
+		if !addr.Is4() {
+			fmt.Printf("Invalid IPv4 addresses %q\n", os.Args[2])
+			os.Exit(2)
+		}
 	}
 
 	iface, err := net.InterfaceByName(os.Args[1])
@@ -28,11 +41,10 @@ func main() {
 
 	p, err := packet.Listen(iface, packet.Raw, rarpProtocol, nil)
 	if err != nil {
-		util.LogFatalAndExit(err, "failed to list on interface %q", os.Args[1])
+		util.LogFatalAndExit(err, "failed to listen on interface %q", os.Args[1])
 	}
 	defer p.Close()
 
-	addr := netip.IPv4Unspecified()
 	arp, err := arp.NewPacket(rarpRequestOp, iface.HardwareAddr, addr, ethernet.Broadcast, addr)
 	if err != nil {
 		util.LogFatalAndExit(err, "failed to create rarp request packet")
